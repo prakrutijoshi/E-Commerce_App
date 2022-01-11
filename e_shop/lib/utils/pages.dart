77dcb856075405b4ae0fe47547d5b1222251386a
prakrutijoshi <joshiprakruti6@gmail.com>
2022-01-11 14:39:55 +0530

    orders navigation changes

--- e_shop/lib/utils/pages.dart
+++ e_shop/lib/utils/pages.dart
@@ -1,11 +1,11 @@
+import 'package:e_shop/presentation/screens/my_orders/my_order_screen.dart';
 import '../presentation/notifications.dart';
 import '../presentation/screens/home/components/body.dart';
-import '../presentation/screens/orderscreen.dart';
 import '../presentation/screens/profile/profile_screen.dart';
 
 final pages = [
   Body(),
-  OrdersScreen(),
+  MyOrderScreen(),
   NotificationsScreen(),
   ProfileScreen(),
 ];
